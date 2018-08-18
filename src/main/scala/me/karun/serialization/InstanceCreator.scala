2b42da7bc3a9638a663f1fbe987aa16781924c1a
Karun Japhet <karun@japhet.in>
2018-08-19 00:13:37 +0530

    Refactor to reduce code

--- src/main/scala/me/karun/serialization/InstanceCreator.scala
+++ src/main/scala/me/karun/serialization/InstanceCreator.scala
@@ -3,37 +3,31 @@ package me.karun.serialization
 import java.lang.reflect.Constructor
 
 case class InstanceCreator[T](clazz: Class[T]) {
-  def newInstance(values: Map[String, Any]): T = {
-    val privateFieldPrefix = s"${clazz.getName.replace(".", "$")}$$$$"
-    val params = clazz.getDeclaredFields
-      .map(_.getName.replace(privateFieldPrefix, ""))
-      .map(values(_))
-      .array
-      .asInstanceOf[Array[_ <: Object]]
-
-    clazz.getConstructors.head.asInstanceOf[Constructor[T]].newInstance(params: _*)
-  }
-
-  private val defaultSuppliers: Map[Class[_], () => _] = Map(classOf[String] -> (() => ""), classOf[Int] -> (() => 0), classOf[Double] -> (() => 0.0))
 
   import InstanceCreator.richClassImplicit
 
+  private val defaultSuppliers: Map[Class[_], () => _] = Map(classOf[String] -> (() => ""), classOf[Int] -> (() => 0), classOf[Double] -> (() => 0.0))
+
+  def newInstance(data: Map[String, Any]): T = clazz.getConstructors.head.asInstanceOf[Constructor[T]].newInstance(params(data = data): _*)
+
   def generateWithTestData(suppliers: Map[Class[_], () => _], data: Map[String, Any] = Map.empty): T = {
     val allSuppliers = defaultSuppliers ++ suppliers
-    val objectCreator = () => {
-      val privateFieldPrefix = s"${clazz.getName.replace(".", "$")}$$$$"
-      val params = clazz.getDeclaredFields
-        .map(f => data.getOrElse(f.getName.replace(privateFieldPrefix, ""), f.getType.generateWithTestData(allSuppliers, data)))
-        .array
-        .asInstanceOf[Array[_ <: Object]]
+    val objectCreator = () => clazz.getConstructors.head.asInstanceOf[Constructor[T]]
+      .newInstance(params(allSuppliers, data): _*)
 
-      clazz.getConstructors.head.asInstanceOf[Constructor[T]].newInstance(params: _*)
-    }
-    val supplier = allSuppliers.getOrElse(clazz, objectCreator)
-    supplier.apply().asInstanceOf[T]
+    allSuppliers.getOrElse(clazz, objectCreator).apply().asInstanceOf[T]
   }
 
   def generateWithTestData(): T = generateWithTestData(defaultSuppliers)
+
+  private def params(suppliers: Map[Class[_], () => _] = Map.empty, data: Map[String, Any] = Map.empty) = {
+    val privateFieldPrefix = s"${clazz.getName.replace(".", "$")}$$$$"
+
+    clazz.getDeclaredFields
+      .map(f => data.getOrElse(f.getName.replace(privateFieldPrefix, ""), f.getType.generateWithTestData(suppliers, data)))
+      .array
+      .asInstanceOf[Array[_ <: Object]]
+  }
 }
 
 object InstanceCreator {
